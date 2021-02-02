ba5db128933856b746f356af89d5398cdc72b28a
PorterOBrien2021 <824085@mystma.org>
2021-02-02 16:20:45 +0000

    count 4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def countA(w):
     count = 0
     for i in range (0, len(w)):
         if w[i] == "a":
-            count = (0 + 1)
+            count = (0 + i)
 
     return count
-print(countA("cat"))
+print(countA("aaaaaa"))
