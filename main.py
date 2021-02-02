0661dc531ea75d715c7257fbbf30155fa44bb11e
PorterOBrien2021 <824085@mystma.org>
2021-02-02 16:09:35 +0000

    count

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    count = 0
+    for i in range (0, len(w)):
+        if w[i] == "a":
+            count = 0 + i
+
+            return count
+print(countA("cat"))
